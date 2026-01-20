114d85a3e344228a6b18c0843086c35a1f37ee7c
Wesleyjjx-dev <wesleydelaordellavega@hotmail.com>
2026-01-19 23:29:52 -0300

    js mais simples

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-// ===== GitHub API =====
-const username = 'wesleyjjx-dev'
+const toggle = document.getElementById("darkModeToggle");
 
-fetch(`https://api.github.com/users/${username}`)
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById('avatar').src = data.avatar_url
-    document.getElementById('bio').textContent = data.bio || 'Bio não informada'
-    document.getElementById('followers').textContent = data.followers
-    document.getElementById('location').textContent = data.location || 'Não informado'
-    document.getElementById('repos').textContent = data.public_repos
-  })
-
-// ===== Dark Mode =====
-const toggleBtn = document.getElementById('themeToggle')
-
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark')
-  localStorage.setItem(
-    'theme',
-    document.body.classList.contains('dark') ? 'dark' : 'light'
-  )
-})
-
-if (localStorage.getItem('theme') === 'dark') {
-  document.body.classList.add('dark')
-}
-
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
